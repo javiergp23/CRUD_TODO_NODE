a25f221a9411b705f2d9385c4746301461124464
Javier Gonzalez <javgon2306@gmail.com>
2024-09-20 09:12:50 -0300

    importing mongoose

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import app  from './app.js'
 import { connectDB } from './db.js'
 
+//Conexión a la base de datos
 connectDB()
+
+//Inicio del servidor
 app.listen(3023)
 console.log('Server running on port ', 3023)
