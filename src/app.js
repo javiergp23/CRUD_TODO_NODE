95eacaa6f6acc3f91742ad031b5952e738bc9f75
Javier Gonzalez <javgon2306@gmail.com>
2024-09-20 11:03:54 -0300

    test-mongo-connect

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import authRoutes from './routes/auth.js'
 const app = express()
 
 app.use(morgan('dev'))
+app.use(express.json());
 app.use("/api",authRoutes)
 
 export default app
--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,2 +1,20 @@
-export const register = (req, res) => res.send('register')
+import userModel from "../models/userModel.js"
+
+export const register = async (req, res) => {
+    // const {email, username, password} = req.body  
+   
+    // try{
+    //     const newUser = new User({
+    //         username,
+    //         email,
+    //         password
+    //     })
+    //     await newUser.save()
+    //     res.send('registrando')
+    // }catch(err){
+    //     res.status(400).send(err)
+    // }
+    console.log(req.body)
+}
+
 export const login = (req, res) => res.send('login')
