a76276dae8076c6d756795139004058646f3b61d
Javier Gonzalez <javgon2306@gmail.com>
2024-09-20 13:12:43 -0300

    creating hash for password

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,18 +1,30 @@
-import User from "../models/userModel.js"
+import User from "../models/userModel.js";
+import bcrypt from 'bcryptjs';
+import  { createAccessToken } from '../libs/jwt.js'
 
 export const register = async (req, res) => {
 
     const {email, username, password} = req.body  
    
     try{
+        const passwordHash = await bcrypt.hash(password, 10)
+
         const newUser = new User({
             username,
             email,
-            password
+            password: passwordHash,
         })
 
         const userSaved = await newUser.save()
-        res.json(userSaved)
+        const token = await createAccessToken({id: userSaved._id})
+        res.cookie('token', token)
+        res.json({
+            id:userSaved._id,
+            username: userSaved.username,
+            email: userSaved.email,
+            createdAt: userSaved.createdAt,
+            updatedAt: userSaved.updatedAt
+        });
     }catch(err){
         res.status(400).send(err)
     }
