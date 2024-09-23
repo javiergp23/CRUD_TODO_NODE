95934495d4b81fbc69ed5439b7daa4989925f880
Javier Gonzalez <javgon2306@gmail.com>
2024-09-23 15:43:00 -0300

    add login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -17,7 +17,7 @@ export const register = async (req, res) => {
         const userSaved = await newUser.save()
 
         const token = await createAccessToken({id: userSaved._id})
-        
+
         res.cookie('token', token)
         res.json({
             id:userSaved._id,
@@ -31,4 +31,37 @@ export const register = async (req, res) => {
     }
 }
 
-export const login = (req, res) => res.send('login')
+export const login = async (req, res) => {
+
+    const {email, password} = req.body  
+    try{
+
+        const userFound = await User.findOne({email});
+        if(!userFound) return res.status(400).json({MESSAGE: "User not found"})
+
+        const isMatch = await bcrypt.compare(password, userFound.password)
+        if(!isMatch) 
+            return res.status(400).json({MESSAGE: "Password not match"})
+
+        const token = await createAccessToken({id: userFound._id})
+        
+        res.cookie('token', token)
+        res.json({
+            id: userFound._id,
+            username:  userFound.username,
+            email: userFound.email,
+            createdAt: userFound.createdAt,
+            updatedAt: userFound.updatedAt
+        });
+    }catch(err){
+        res.status(400).send(err)
+    }
+}
+
+export const logout = (req, res) => {
+    res.cookie('token', '', {
+        expires: new Date(0),
+    })
+    return res.sendStatus(200)
+}
+
