acce1a8ef09793e727c4925c56470b210e760dc9
Javier Gonzalez <javgon2306@gmail.com>
2024-09-23 16:36:38 -0300

    add tasks.routes protect

--- src/app.js
+++ src/app.js
@@ -3,12 +3,17 @@ import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
 
 import authRoutes from './routes/auth.js';
+import tasksRoutes from './routes/tasks.routes.js';
 
 const app = express()
 
 app.use(morgan('dev'))
 app.use(express.json());
-app.use("/api",authRoutes)
+//Reading cookies
 app.use(cookieParser());
+
+//routes
+app.use("/api",authRoutes)
+app.use("/api", tasksRoutes)
 
 export default app;
--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -1,5 +1,17 @@
+import jwt from 'jsonwebtoken';
+import { TOKEN_SECRET } from '../config.js';
+
 export const authRequired = (req, res, next) => {
-    const cookies = req.cookies;
-    console.log(cookies)
-    next()
+    const { token}  = req.cookies;
+    if(!token) return res.status(401).json({MESSAGE: "No token provided"})
+    
+    jwt.verify(token, TOKEN_SECRET, (err, decoded) => {
+        if(err) return res.status(401).json({MESSAGE: "Token not valid"})
+        
+        req.decoded = decoded; 
+
+        next()
+    })
+
+    
 }
