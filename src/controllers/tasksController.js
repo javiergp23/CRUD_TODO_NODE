543ba567aac825a616255252d131f86c7d1eab04
Javier Gonzalez <javgon2306@gmail.com>
2024-09-24 15:14:46 -0300

    changes tasksController

--- src/controllers/tasksController.js
+++ src/controllers/tasksController.js
@@ -2,8 +2,3 @@ export const getTasks = async (req, res) => {};
 
 export const createTasks = async (req, res) => {};
 
-export const getTask = async (req, res) => {};
-
-export const updateTasks = async (req, res) => {};
-
-export const deleteTasks = async (req, res) => {};
